98523a02492a3f68d3f314ad500e79fa19dbf8be
Pudgeros <97166337+Pudgeros@users.noreply.github.com>
2025-06-05 11:53:54 +0500

    Update Task3_sequential.cpp

--- Lab2/Task3/Task3_sequential.cpp
+++ Lab2/Task3/Task3_sequential.cpp
@@ -5,7 +5,7 @@
 #include <chrono>
 
 int main() {
-    const size_t N = 1000000;
+    const size_t N = 100000000;
     std::vector<float> A(N), B(N), sum(N), diff(N), prod(N), div(N);
 
     std::srand(static_cast<unsigned>(std::time(nullptr)));
